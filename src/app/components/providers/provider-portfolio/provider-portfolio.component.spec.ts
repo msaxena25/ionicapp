6b20af197876f9a2afcec7887ae5a591838be407
mohit_compunnel <mohit.saxena@compunnel.in>
2019-05-15 12:22:31 +0530

    added povider portfolio component

---
+++ src/app/components/providers/provider-portfolio/provider-portfolio.component.spec.ts
@@ -0,0 +1,27 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ProviderPortfolioComponent } from './provider-portfolio.component';
+
+describe('ProviderPortfolioComponent', () => {
+  let component: ProviderPortfolioComponent;
+  let fixture: ComponentFixture<ProviderPortfolioComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ProviderPortfolioComponent ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ProviderPortfolioComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/providers/provider-portfolio/provider-portfolio.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-provider-portfolio',
+  templateUrl: './provider-portfolio.component.html',
+  styleUrls: ['./provider-portfolio.component.scss'],
+})
+
+// This is complete read only page of Provider profile data that will be visible for User only 
+export class ProviderPortfolioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
