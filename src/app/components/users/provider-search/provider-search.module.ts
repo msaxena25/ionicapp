3e107e4be4e6af5d8f786444b505148e32e866e6
mohit_compunnel <mohit.saxena@compunnel.in>
2019-05-16 18:35:21 +0530

    added select fielf to select a service option

--- src/app/components/users/provider-search/provider-search.module.ts
+++ src/app/components/users/provider-search/provider-search.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { ProviderSearchComponent } from './provider-search.component';
 import { RouterModule } from '@angular/router';
 import { Geolocation } from '@ionic-native/geolocation/ngx';
 import { NativeGeocoder, NativeGeocoderOptions } from '@ionic-native/native-geocoder/ngx';
+import { AppCommonModule } from 'src/app/shared/app-common/app-common.module';
 
 
 const routes = [
@@ -16,7 +16,7 @@ const routes = [
 @NgModule({
   declarations: [ProviderSearchComponent],
   imports: [
-    CommonModule,
+    AppCommonModule,
     RouterModule.forChild(routes)
   ],
   providers: [Geolocation, NativeGeocoder]
