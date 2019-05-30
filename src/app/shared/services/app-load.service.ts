f8da93129dacf3ea4b0925b0046be09534f49c14
mohitsaxena <mohitsaxena2507@gmail.com>
2019-05-30 12:47:17 +0530

    added code for device back button - TODO

--- src/app/shared/services/app-load.service.ts
+++ src/app/shared/services/app-load.service.ts
@@ -1,16 +1,16 @@
 import { Injectable } from '@angular/core';
 import { Storage } from '@ionic/storage';
 import { SharedService } from './shared.service';
+import { Platform } from '@ionic/angular';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AppLoadService {
 
-  constructor(private storage: Storage, private sharedService: SharedService) { }
+  constructor(private storage: Storage, private sharedService: SharedService, private platform: Platform) { }
 
   async initializeApp() {
-    // Or to get a key/value pair
     await this.storage.get('isUserLoggedIn').then((val) => {
       if (val) {
         console.log('User is logged In', val);
@@ -18,6 +18,18 @@ export class AppLoadService {
       }
 
     });
-    console.log('app initialized');
+
+    // For future use
+    this.platform.ready().then(() =>{ 
+
+      this.platform.backButton.subscribe(() => { 
+        // code that is executed when the user pressed the back button
+        // and ionic doesn't already know what to  
+      //  alert('hi pressed back button');
+      });
+    //  console.log('app initialized');
+    })
+
+  
   }
 }
