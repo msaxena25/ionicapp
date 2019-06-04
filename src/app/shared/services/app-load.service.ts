18d3151a64d652563bcadfed6a43cefd0fe09bb4
mohitsaxena <mohitsaxena2507@gmail.com>
2019-06-04 18:08:37 +0530

    implemted android-permissions -  TODO need to test

--- src/app/shared/services/app-load.service.ts
+++ src/app/shared/services/app-load.service.ts
@@ -2,34 +2,66 @@ import { Injectable } from '@angular/core';
 import { Storage } from '@ionic/storage';
 import { SharedService } from './shared.service';
 import { Platform } from '@ionic/angular';
+import { AndroidPermissions } from '@ionic-native/android-permissions/ngx';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AppLoadService {
 
-  constructor(private storage: Storage, private sharedService: SharedService, private platform: Platform) { }
+  constructor(private storage: Storage, private sharedService: SharedService, private platform: Platform,
+    private androidPermissions: AndroidPermissions) {
+    this.platform.ready().then(() => {
+
+      this.androidPermissions.checkPermission(this.androidPermissions.PERMISSION.SEND_SMS).then(
+        result => {
+          alert('Has permission? ' + result.hasPermission)
+          if (!result.hasPermission) {
+            this.androidPermissions.requestPermissions([this.androidPermissions.PERMISSION.SEND_SMS, this.androidPermissions.PERMISSION.CAMERA])
+              .then((result) => { alert('granted permission:  ' + result) }, error => { alert(error) });
+          }
+        },
+        err => this.androidPermissions.requestPermission(this.androidPermissions.PERMISSION.SEND_SMS)
+      );
+
+      /*   this.androidPermissions.PERMISSION.READ_EXTERNAL_STORAGE,
+        this.androidPermissions.PERMISSION.WRITE_EXTERNAL_STORAGE
+        this.androidPermissions.PERMISSION.RECEIVE_SMS,
+        this.androidPermissions.PERMISSION.CALL_PHONE, */
+
+      // this.androidPermissions.requestPermissions(
+      //   [
+      //     this.androidPermissions.PERMISSION.CAMERA,
+
+
+      //     this.androidPermissions.PERMISSION.SEND_SMS,
+
+      //   ]
+      //     ).then((result) => { alert('granted permission:  ' + result) }, error => { alert(error) });
+
+    })
+  }
 
   async initializeApp() {
     await this.storage.get('isUserLoggedIn').then((val) => {
       if (val) {
         console.log('User is logged In', val);
-       this.sharedService.isUserLoggedIn = true;
+        this.sharedService.isUserLoggedIn = true;
       }
 
     });
 
     // For future use
-    this.platform.ready().then(() =>{ 
+    this.platform.ready().then(() => {
 
-      this.platform.backButton.subscribe(() => { 
+      this.platform.backButton.subscribe(() => {
         // code that is executed when the user pressed the back button
         // and ionic doesn't already know what to  
-      //  alert('hi pressed back button');
+        //  alert('hi pressed back button');
       });
-    //  console.log('app initialized');
+      //  console.log('app initialized');
     })
 
-  
+
   }
 }
